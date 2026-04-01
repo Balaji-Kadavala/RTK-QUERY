8cd746a70bf853b7714d576b48710fc3dccc9c8c
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-01 14:42:29 +0530

    Products API call with RTK Query

--- src/features/Products/products.jsx
+++ src/features/Products/products.jsx
@@ -1,11 +1,15 @@
 import { useGetAllProductsQuery } from "../../services/productsApi";
 
 function Products() {
-    const {isLoading,data}=useGetAllProductsQuery();
+    const { isLoading, data } = useGetAllProductsQuery();
     return (<div>
         <h1>Products</h1>
         {isLoading && <h3>Loading....</h3>}
-        {isLoading && console.log(data)}
+        {!isLoading && <ul>
+            {data?.products.map((product) => {
+                return <li>{product.title}</li>
+            })}
+        </ul>}
     </div>)
 }
 
