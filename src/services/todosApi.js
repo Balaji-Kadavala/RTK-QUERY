8e65c323b15391a3d98586b818fc89670492f135
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-01 16:04:33 +0530

    todosApi changed

--- src/services/todosApi.js
+++ src/services/todosApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const todosApi = createApi({
   reducerPath: 'todosApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5173' }),
   endpoints: (build) => ({
     getAllTodos: build.query({
       query: () => `/todos`,
