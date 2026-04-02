25ff073aa3f6da24a8c25d052f5c6f428261d432
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-02 14:56:35 +0530

    Completed TodoList using JSON Server with RTK-Query

--- src/features/todolist/todolist.jsx
+++ src/features/todolist/todolist.jsx
@@ -1,15 +1,71 @@
-import { useGetAllTodosQuery } from "../../services/todosApi";
+import { useRef, useState } from "react";
+import { useAddNewTodoMutation, useGetAllTodosQuery, useLazyGetAllTodosQuery, useUpdateTodoMutation, useDeleteTodoMutation } from "../../services/todosApi";
 
 function Todolist() {
-    const { isLoading, data } = useGetAllTodosQuery()
+    const { isLoading, data: todos } = useGetAllTodosQuery();
+    const [ntd, setNtd] = useState();
+    const [addNewTodoFn] = useAddNewTodoMutation();
+    const [LazyGetAllTodos] = useLazyGetAllTodosQuery();
+    const [updateTodoFn] = useUpdateTodoMutation();
+    const [deleteTodoFn] = useDeleteTodoMutation();
+    const [selectedTodo, setSelectedTodo] = useState(null);
+    const inp = useRef();
+    function handleAddNewTodo() {
+        addNewTodoFn({ "title": ntd, "status": "not completed" }).then(()=>{
+        LazyGetAllTodos();
+        });
+    }
+    function undoTodo(todo) {
+        const updatedTodo = { ...todo, status: "not completed" }
+        updateTodoFn(updatedTodo).then(()=>{
+        LazyGetAllTodos();
+        });
+    }
+    function doneTodo(todo) {
+        const updatedTodo = { ...todo, status: "completed" }
+        updateTodoFn(updatedTodo).then(()=>{
+        LazyGetAllTodos();
+        });
+    }
+    function handleDeleteTodo(todo) {
+        deleteTodoFn(todo.id).then(()=>{
+        LazyGetAllTodos();
+        });
+    }
+    function handleEditTodo(todo) {
+        setSelectedTodo(todo);
+        inp.current.focus();
+        inp.current.value = todo.title;
+    }
+    function handleUpdateTodo(){
+        const updatedTodo={...selectedTodo, title:ntd}
+        updateTodoFn(updatedTodo).then(()=>{
+            LazyGetAllTodos();
+            setSelectedTodo(null);
+            inp.current.value="";
+        })
+    }
     return (
         <div>
             <h1>Todolist</h1>
+            <input type="text" placeholder="Enter todo" onChange={(ev) => setNtd(ev.target.value)} ref={inp} />
+            {!selectedTodo && <button onClick={() => {
+                handleAddNewTodo()
+            }}>Add Todo</button>}
+            {selectedTodo && <button onClick={()=>handleUpdateTodo()}>Update Todo</button>}
             {isLoading && <h3>Loading...</h3>}
             {!isLoading &&
                 <ul>
-                    {data?.todos.map(todo=>{
-                        return <li>{todo}</li>
+                    {todos?.map(todo => {
+                        return <li style={{ textDecoration: todo.status === 'completed' ? 'line-through' : 'none' }}>
+                            {todo.title}
+                            <button onClick={() => {
+                                handleEditTodo(todo);
+                            }}>Edit</button>
+                            {todo.status == "completed" && <button onClick={() => undoTodo(todo)}>Undo</button>}
+                            {todo.status == "not completed" && <button onClick={() => doneTodo(todo)}>Done</button>}
+                            <button onClick={() => handleDeleteTodo(todo)}>Delete</button>
+                        </li>
                     })}
                 </ul>
             }
--- src/services/todosApi.js
+++ src/services/todosApi.js
@@ -3,14 +3,40 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const todosApi = createApi({
   reducerPath: 'todosApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5173' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
   endpoints: (build) => ({
     getAllTodos: build.query({
       query: () => `/todos`,
     }),
+    addNewTodo: build.mutation({
+      query:(newTodo)=>{
+        return {
+          url:"/todos",
+          method:"POST",
+          body:{...newTodo}
+        }
+      }
+    }),
+    updateTodo: build.mutation({
+      query:(todo)=>{
+        return {
+          url:`/todos/${todo.id}`,
+          method:"PUT",
+          body:{...todo}
+        }
+      }
+    }),
+    deleteTodo: build.mutation({
+      query:(id)=>{
+        return {
+          url:`/todos/${id}`,
+          method:"DELETE"
+        }
+      }
+    })
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetAllTodosQuery } = todosApi;
+export const { useGetAllTodosQuery, useAddNewTodoMutation, useLazyGetAllTodosQuery, useUpdateTodoMutation, useDeleteTodoMutation } = todosApi;
